3c908fe52915c3c1ebdcf995e714b7de3183bcc9
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-05-10 17:38:54 +0200

    bump to '1.1.2-dev.1'

--- package.py
+++ package.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Package declaring addon version."""
 name = "ayon_ocio"
-version = "1.1.1"
+version = "1.1.2-dev.1"
 title = "OCIO Distribution"
 client_dir = "ayon_ocio"
 
